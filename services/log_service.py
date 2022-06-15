917c77d5013be7da3da7125f11eecaeaae40c8e9
Ty Bui <67993925+thaibui2308@users.noreply.github.com>
2022-06-15 17:45:10 +0700

    working on logging service

--- services/log_service.py
+++ services/log_service.py
@@ -1,3 +1,30 @@
+import logging
 #TODO: Implement a logging service
 class LogService():
-    pass
+    def __init__(self, GlobalStat, logLevel="DEFAULT", timestamp = None, filename = None):
+        self.filename = filename
+        self.Stat = GlobalStat
+        self.logLevel = logLevel
+        self.timestamp = timestamp
+    
+    def create_log_file(self):
+        if self.filename is None:
+            self.filename = 'log.txt'
+            
+        with open(self.filename, 'w') as f:
+            return f is None
+        
+        return False
+    
+    def setLogLevel(self, logLevel):
+        self.logLevel
+    
+    def log(self, message):
+        content = '[{} {}]: {}'.format(self.logLevel, self.timestamp, message)
+        return content
+    
+    def commit(self, message): 
+        with open(self.filename, 'w+') as f:
+            f.write(message)
+        
+    
